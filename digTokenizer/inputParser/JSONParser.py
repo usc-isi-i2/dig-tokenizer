90a63081be5ee6a27b9a5f2074569202bf69f444
Dipsy Kapoor <dipsykapoor@gmail.com>
2015-11-06 13:05:13 -0800

    fix JSONParser

--- digTokenizer/inputParser/JSONParser.py
+++ digTokenizer/inputParser/JSONParser.py
@@ -11,7 +11,9 @@ class JSONParser:
                 self.column_paths.insert(int(index), config["fieldConfig"][index]["path"])
 
     def parse(self, x):
-        return self.parse_with_paths(x, "uri", self.column_paths)
+        x = self.parse_with_paths(x, "uri", self.column_paths)
+        #print "\n\n\nReturn:", x[0], "value:", x[1]
+        return (x[0], x[1])
 
     def parse_with_key(self, x, key_name):
         return self.parse_values_with_paths(x, key_name, self.column_paths)
@@ -20,7 +22,6 @@ class JSONParser:
         json_data = json.loads(x)
         value = self.__extract_columns(json_data, paths)
         key = json_data[key_name]
-        print "Return:", key, "value:", value
         return key, value
 
     def parse_values(self, x):
@@ -28,34 +29,49 @@ class JSONParser:
 
     def parse_values_with_paths(self, x, paths):
         json_data = json.loads(x)
-        return self.__extract_columns(json_data, paths)
+        return list(self.__extract_columns(json_data, paths))
 
     def __extract_columns(self, row, paths):
-        start = self.to_list(row)
-        found = True
-        for path_elem in paths:
-            start = self.__extract_elements(start, path_elem)
-            if len(start) == 0:
-                found = False
-                break
+        for path in paths:
+            x = list(self.__extract_at_path(row, path))
+            if(len(x) > 0):
+                yield ' '.join(x)
 
-        if found:
-            if isinstance(start, list):
-                for elem in start:
-                   yield elem
+    def __extract_at_path(self, row, path):
+            start = self.to_list(row)
+            found = True
+            path_elems = path.split(".")
+            for path_elem in path_elems:
+                if not isinstance(start, list):
+                    break
+                start = self.__extract_elements(start, path_elem)
+                if len(start) == 0:
+                    found = False
+                    break
 
-            else:
-                yield start
+
+            #print "FOUND:", found, ":", start
+            if found:
+                if isinstance(start, list):
+                    for elem in start:
+                       yield elem
+
+                else:
+                    yield start
 
     def __extract_elements(self, array, elem_name):
         result = []
+
         for elem in array:
             if elem_name in elem:
+                #print "Elem:", elem, "elem_name:", elem_name
                 elem_part = elem[elem_name]
                 if isinstance(elem_part, list):
                     result.extend(elem_part)
                 else:
                     result.append(elem_part)
+
+        #print "\n\nFind ", elem_name, "in", array, "\nResult:", result
         return result
 
 
