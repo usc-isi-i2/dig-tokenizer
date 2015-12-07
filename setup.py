b864026e20cac40877af85aed8717219251d539d
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-07 14:18:34 -0800

    should be buildable locally using python setup.py develop

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'digTokenizer',
+    'description': 'digTokenizer',
+    'author': 'Rajagopal Bojanapalli',
+    'url': 'https://github.com/usc-isi-i2/dig-tokenizer',
+    'download_url': 'https://github.com/usc-isi-i2/dig-tokenizer',
+    'author_email': 'rajagopal067@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose2',
+                         'py4j',
+                         'pyspark',
+                         'digEntityMerger'],
+    # these are the subdirs of the current directory that we care about
+    'packages': ['digTokenizer'],
+    'scripts': [],
+}
+
+setup(**config)
