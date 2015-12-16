e63156ca75f2a195674ac9281f7e93f598e5d164
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-16 14:29:57 -0800

    final

--- digTokenizer/test/testTokenizer.py
+++ digTokenizer/test/testTokenizer.py
@@ -9,6 +9,7 @@ import argparse
 from digTokenizer.tokenizer import Tokenizer
 from digSparkUtil.fileUtil import FileUtil
 from digSparkUtil.dictUtil import as_dict, dict_minus
+from digSparkUtil.miscUtil import seconds_since_epoch
 
 def testTokenizer(sc, input, output, config,
                   limit=None, 
@@ -16,16 +17,16 @@ def testTokenizer(sc, input, output, config,
                   debug=0, 
                   location='hdfs',
                   inputFileFormat="sequence",
-                  inputDataFormat="json",
+                  inputDataType="json",
                   outputFileFormat="sequence",
-                  outputDataFormat="json",
+                  outputDataType="json",
                   verbose=False,
                   **kwargs):
 
     futil = FileUtil(sc)
 
     # LOAD DATA
-    rdd_ingest = futil.load_file(input, inputFileFormat, inputDataFormat)
+    rdd_ingest = futil.load_file(input, file_format=inputFileFormat, data_type=inputDataType)
     rdd_ingest.setName('rdd_ingest_input')
 
     # LIMIT/SAMPLE (OPTIONAL)
@@ -38,24 +39,28 @@ def testTokenizer(sc, input, output, config,
         ratio = float(limit) / rdd_ingest.count()
         rdd_ingest = rdd_ingest.sample(False, ratio, seed=sampleSeed)
         
-    tokOptions = {"verbose": verbose}
+    tokOptions = {"verbose": verbose,
+                  "file_format": inputFileFormat,
+                  "data_type": inputDataType}
     tokenizer = Tokenizer(config, **tokOptions)
     rdd_tokenized = tokenizer.perform(rdd_ingest)
 
+    rdd_tokenized.saveAsTextFile('/tmp/rdd_tokenized_' + str(seconds_since_epoch()))
+
     # SAVE DATA
     outOptions = {}
-    futil.save_file(rdd_tokenized, output, outputFileFormat, outputDataFormat, **outOptions)
+    futil.save_file(rdd_tokenized, output, file_format=outputFileFormat, data_type=outputDataType, **outOptions)
 
 def main(argv=None):
     '''this is called if run from command line'''
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--input', required=True)
     parser.add_argument('--inputFileFormat', default='sequence', choices=('text', 'sequence'))
-    parser.add_argument('--inputDataFormat', default='json', choices=('json', 'csv'))
+    parser.add_argument('--inputDataType', default='json', choices=('json', 'csv'))
 
     parser.add_argument('-o','--output', required=True)
     parser.add_argument('--outputFileFormat', default='sequence', choices=('text', 'sequence'))
-    parser.add_argument('--outputDataFormat', default='json', choices=('json', 'csv'))
+    parser.add_argument('--outputDataType', default='json', choices=('json', 'csv'))
 
     parser.add_argument('--config', default=None)
 
@@ -69,8 +74,12 @@ def main(argv=None):
     sparkName = "testTokenizer"
     sc = SparkContext(appName=sparkName)
 
-    testTokenizer(sc, args.input, args.output, args.config, 
-                  **dict_minus(as_dict(args), "config")
+    kwargs = {"data_type": args.inputDataType,
+              "file_format": args.inputFileFormat,
+              "limit": args.limit,
+              "verbose": args.verbose}
+
+    testTokenizer(sc, args.input, args.output, args.config, **kwargs)
 
 # call main() if this is run as standalone
 if __name__ == "__main__":
